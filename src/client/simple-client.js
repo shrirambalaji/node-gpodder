6951fe1e4779d9fd752796156dda26f28201461f
Shriram Balaji <>
2018-05-10 19:25:11 +0530

    Adding babelrc config, basic subscriptions api template and tests

--- src/client/simple-client.js
+++ src/client/simple-client.js
@@ -1,12 +1,24 @@
 const path = require("path");
-const HOMEDIR = path.join(__dirname, "..", "..")
-const api = require(path.join(HOMEDIR))
-class SimpleClient {
-	constructor(username, password, host) {
-		this.username = username;
-		this.password = password;
-		this.host = host;
-	}
+const HOMEDIR = path.join(__dirname, "..", "..");
+const SRCDIR = path.join(HOMEDIR, "src");
+const auth = require(path.join(SRCDIR, ""));
+
+function SimpleClient(username, password, host) {
+  this.username = username;
+  this.password = password;
+  this.host = host;
+}
+
+SimpleClient.prototype.getSubscriptions = () => {
+  // get subscriptions for the specified user
+};
+
+SimpleClient.prototype.putSubscriptions = () => {
+  // put subscriptions
+};
+
+SimpleClient.prototype.getSuggestions = () => {
+  // get Suggestions
 };
 
 module.exports = SimpleClient;
