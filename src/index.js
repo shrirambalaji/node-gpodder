9e2df51a2e43824850fe409de6b4bbf6ed0f804b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-01-23 23:54:14 +0530

    Expose Api in entry file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 const path = require("path");
 const { SimpleClient } = require("./client/simple-client");
 const { AdvancedClient } = require("./client/advanced-client");
-
+const Api = require('./api')
 module.exports = {
 	SimpleClient,
-	AdvancedClient
+	AdvancedClient,
+	Api
 };
-// export default "./api";
